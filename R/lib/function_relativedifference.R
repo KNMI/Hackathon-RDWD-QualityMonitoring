fe0d434577301aafa532a2d6f8c7096c92aa5ec2
LottedeVos <lotte.devos@wur.nl>
2017-06-13 14:55:45 +0000

    Because the output is not stored, we do not change NA values into -9999 values. meta information is added.

--- R/lib/function_relativedifference.R
+++ R/lib/function_relativedifference.R
@@ -1,5 +1,6 @@
-#' @title 
-#' @description 
+#' @title Relative difference of timeseries
+#' @description Calculate the relative difference between AWS timeserie and Man timeserie.
+#' @details the input should have certain column names and identical timeseries. If not, the function stops with an error that identifies the problem. 
 #' @example input; files <- grab.test.data(); AWS_timeserie <- files$Almelo_664_N_seasonal_precip[,c(1,3)]; Man_timeserie <- files$Enschede_665_N_seasonal_precip[,c(1,3)]; names(AWS_timeserie) <- names(Man_timeserie) <- c("time", "average")
 #' @param AWS_timeserie The input contains 1 timecolumn and only 1 value column with AWS precipitation averages named 'average', and NA values are indicated with '-9999'.
 #' @param Man_timeserie The input contains 1 timecolumn and only 1 value column with Man precipitation averages named 'average', and NA values are indicated with '-9999'.
@@ -16,10 +17,10 @@ relative.difference <- function(AWS_timeserie, Man_timeserie){
   Man_timeserie$average[which(Man_timeserie$average == -9999)] <- NA
   
   relative_dif <- 100 * (Man_timeserie$average - AWS_timeserie$average) / AWS_timeserie$average
-  relative_dif[which(is.na(relative_dif))] <- -9999
+ # relative_dif[which(is.na(relative_dif))] <- -9999   # in case we save in the meantime turn on. 
   output <- data.table(time=Man_timeserie$time, relative_dif=relative_dif)
   
   return(output)
 }
 
-relative.difference(AWS_timeserie, Man_timeserie)
+
