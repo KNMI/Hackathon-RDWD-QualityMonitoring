f05709a70dead815131b3844cf552efbc90577eb
Wim van Moosel <moosel@birdexp06.knmi.nl>
2017-06-13 10:52:09 +0000

    First steps in structuring the information layout for the webpage

--- R/shiny/ui.R
+++ R/shiny/ui.R
@@ -1,35 +1,45 @@
 library(shiny)
 
-#title above navbar
+# Run using:
+# runApp("~/Hackathon-RDWD-QualityMonitoring/R/shiny")
 
 fluidPage(
 
+  #title above navbar
   h1("Inhomogenity detection", align = "center"),
 
-navbarPage("",
-           tabPanel("Alerts",
-                    fluidRow(
-                      
-                      includeMarkdown("Warning_example.Rmd")
-                      ),
-                    
-                    
-                    
-                    fluidRow(
-                      
-                      column(6,class = "well",style = "background-color:red",plotOutput("plot")),
-                      column(6,h4("quantiles"),
-                             tableOutput("summary"))
-                    )),
-                    tabPanel("Background information",
-                    fluidRow(
-                      
-                        includeMarkdown("Background_info_example.Rmd")
-                        )
-           )
-
-           )
-
+  navbarPage("",
+    tabPanel("Alerts",
+      fluidRow(
+        includeMarkdown("Warning_example.Rmd")
+      ),
+      fluidRow(
+        column(6,
+          div(class = "panel panel-danger", 
+            div(class="panel-heading", "title"),
+            div(class="panel-body", 
+              div("Body"),
+              div(plotOutput("plot"))
+            )
+          )
+        ),
+        column(6,
+          h4("quantiles"),
+          tableOutput("summary")
+        )
+      )
+    ),
+    tabPanel("Overview",
+      fluidRow(
+        p("table: red/green, plotje on white board")
+      )
+    ),
+    tabPanel("Background information",
+      fluidRow(
+        includeMarkdown("Background_info_example.Rmd")
+      )
+    )
+  )
 )
 
            
