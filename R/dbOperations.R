64726c92fd3186381ac38ab297a133d8b9d9853e
Jurian <jurian@jurianbaas.nl>
2017-06-14 14:28:18 +0000

    Database connection now works for testing Added script for workflow testing

--- R/dbOperations.R
+++ R/dbOperations.R
@@ -1,4 +1,5 @@
 library(RMySQL)
+library(data.table)
 
 setup.db <- function() {
   
@@ -12,12 +13,64 @@ setup.db <- function() {
             port = cfg$port)
 }
 
-query.hourly <- function(db) {
+
+query.hourly <- function(db, id = NA) {
+  
+  max.qc <- c(0,1)
+  table.name <- "1hour_validated_"
+  element.name <- "rh"
   
   # This is just a test example for now
-  query <- sprintf("SELECT * FROM %t", "types")
+  query <- sprintf (
+    paste("SELECT",
+          "validated.data_id AS data_id, types.type_id AS type_id, elements.element_id AS element_id, stations.code AS code,",
+          "DATE_FORMAT(date, '%%Y%%m%%d%%H%%i%%s') AS date, value, qc, aggregation, type, name, latitude, longitude, elevation, element_group, elements.description AS element_desc, types.description AS type_desc, element, scale, unit",
+          "FROM %s%s AS validated",
+          "INNER JOIN series ON validated.data_id = series.data_id",
+          "INNER JOIN stations ON series.type_id = stations.type_id AND series.code = stations.code",
+          "INNER JOIN types ON series.type_id = types.type_id",
+          "INNER JOIN elements ON series.element_id = elements.element_id",
+          ";"
+          ),
+    table.name, 
+    element.name)
   
-  ref <- dbEscapeStrings(db, query)
-  data = fetch(ref)
+  #query.safe <- dbEscapeStrings(db, query)
   
+  data.ref <- dbSendQuery(db, query)
+  
+  result <- fetch(data.ref)
+  
+  obj <- list()
+  class(obj) <- "mqm.data.container"
+  
+  obj$meta <- by(result, factor(result$data_id), function(x){
+    data.table(
+      sta_id = unique(x$code),
+      sta_name = unique(x$name),
+      sta_lat = unique(x$latitude),
+      sta_lon = unique(x$longitude),
+      sta_elev = unique(x$elevation),
+      sta_type = ifelse(unique(x$type) == "H", "AWS", "MAN"),
+      area_sta_id = unique(x$code),
+      area_lat = unique(x$latitude),
+      area_lon = unique(x$longitude),
+      area_radius = 0,
+      area_cat = 0,
+      var_id = unique(x$element),
+      var_name = unique(x$element_group),
+      var_unit = unique(x$unit),
+      var_scale = unique(x$scale),
+      var_period = unique(x$aggregation),
+      ser_current = x[nrow(x),"date"]
+    )
+  })
+  names(obj$meta) <- as.character(as.integer(names(obj$meta)) + 1000000)
+  obj$hourly <- by(result, factor(result$data_id), function(x){
+    data.table(datetime = x$date, value = x$value)
+  })
+  names(obj$hourly) <- as.character(as.integer(names(obj$hourly)) + 1000000)
+  
+  
+  return(obj)
 }
