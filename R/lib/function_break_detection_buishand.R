5053218224e11d280e061593d7f5eb3883b7f584
AntoSq <anto.squintu@gmail.com>
2017-06-14 13:11:12 +0000

    adaptation of the function in order to take the series id as an argument
    adaptation of the function in order to read a date as imput and transform it into a year

--- R/lib/function_break_detection_buishand.R
+++ R/lib/function_break_detection_buishand.R
@@ -3,22 +3,22 @@
 #' @author Antonello Squintu
 
 
-break.detection <- function(input){
+break.detection <- function(id,input){
 
 #Package "climtrends" is needed
 library("climtrends")
 
 #Uploading of dummy series. Comment out when working in flow
-# setwd("/home/squintu/Hackathon-RDWD-QualityMonitoring")
-# files <- readRDS("data/testdata.rda")
-# diff_files = files[which(grepl('stump',files))]
-# diff=read.table(diff_files)
-  
+setwd("/home/squintu/Hackathon-RDWD-QualityMonitoring/data")
+#files <- readRDS("data/testdata.rda")
+#diff_files = files[which(grepl('stump',files))]
+#diff=read.table(diff_files)
+#input=read.table("Marknesse_diff_DJF.txt") 
 #Setting of the input table  
 diff=input
-colnames(diff)=c('year','diff')
-diff$year=as.numeric(diff$year)
-
+colnames(diff)=c('datetime','diff')
+diff$time <- strptime(diff$datetime, format="%Y%m%d%H%M%S")
+diff$year <- as.integer(format((strptime(diff$datetime, format="%Y%m%d%H%M%S")-9*3600),'%Y'))
 
 #Buishand test
 bd_buishand=data.frame(year=diff$year)
@@ -44,9 +44,10 @@ for(i in 1:nrow(cy))
 }
 
 plot(bd_buishand$year,bd_buishand$score,type='l',
-     main='Buishand Test',
+     main=paste('Buishand Test',id),
      xlab='year',
-     ylab='buishand score')
+     ylab='buishand score',
+     ylim=c(min(-cv,bd_buishand$score),max(cv,bd_buishand$score)))
 points(bd_buishand$year,bd_buishand$score,pch=16)
 abline(h=cv,col='darkgreen',lwd=2)
 abline(h=-cv,col='darkgreen',lwd=2)
