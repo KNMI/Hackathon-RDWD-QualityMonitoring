96492e379bf768d1a4ab5f06f6a25ba9e45fa53a
Jurian <jurian@jurianbaas.nl>
2017-06-15 13:47:16 +0000

    Changed db call to validated data

--- R/runScripts.R
+++ R/runScripts.R
@@ -12,7 +12,7 @@ source("R/breakDetection.R")
 
  # Input data #
 db <- db.setup()
-obj <- db.query(db, "day", "derived", "rd")
+obj <- db.query(db, "hour", "validated", "rh")
 db.close(db)
 
  # Aggregate AWS hourly values in 8-8 daily values #
