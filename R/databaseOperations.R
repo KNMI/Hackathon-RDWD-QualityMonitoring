c2e4ee4781c9459357bba4067771ec53b30ee315
Jurian <jurian@jurianbaas.nl>
2017-06-19 10:35:26 +0000

    Added WIP version of db.select.timeseries

--- R/databaseOperations.R
+++ R/databaseOperations.R
@@ -50,7 +50,7 @@ db.select.all <- function(db, time.period, station.type, element.name) {
           "data.data_id AS data_id, types.type_id AS type_id, elements.element_id AS element_id, stations.code AS code,",
           "DATE_FORMAT(date, %%s) AS date, value, qc, aggregation, type, name, latitude, longitude, elevation, element_group, elements.description AS element_desc, types.description AS type_desc, element, scale, unit",
           "FROM 1%s_%s_%s AS data",
-          "INNER JOIN series ON data = series.data_id",
+          "INNER JOIN series ON data.data_id = series.data_id",
           "INNER JOIN stations ON series.type_id = stations.type_id AND series.code = stations.code",
           "INNER JOIN types ON series.type_id = types.type_id",
           "INNER JOIN elements ON series.element_id = elements.element_id",
@@ -145,7 +145,7 @@ db.select.all <- function(db, time.period, station.type, element.name) {
   return(obj)
 }
 
-db.select.timeseries <- function(db, seriesID, time.period, station.type, element.name) {
+db.select.timeseries <- function(db, stationIDs, time.period, station.type, element.name) {
   
   #--------------------------------------#
   ### Check the arguments for validity ###
@@ -156,25 +156,27 @@ db.select.timeseries <- function(db, seriesID, time.period, station.type, elemen
   
   if(!station.type %in% supported.station.types) stop(paste("Unsupported station type:", station.type))
   if(!time.period %in% supported.time.periods) stop(paste("Unuspported time period:", time.period))
-  if(length(seriesID) == 0) stop("No series ID(s) given")
+  if(length(stationIDs) == 0) stop("No station ID(s) given")
   
   cfg <- config::get(file = "config/config.yml")
   
   max.qc <- cfg$qc.threshold
   db.na.value <- cfg$database.na.value
   
-  #------------------------------------------#
-  ### Firstly we query the timeseries data ###
-  #------------------------------------------#
+  #-------------------------------------------------------------#
+  ### Query the DB for timeseries data from specific stations ###
+  #-------------------------------------------------------------#
   
   query <- sprintf(paste(
-    "SELECT data_id AS id, DATE_FORMAT(date, %%s) AS datetime, value, qc",
-    "FROM 1%s_%s_%s AS data",
-    "WHERE data.data_id IN (%s)"),
+    "SELECT data.data_id AS id, DATE_FORMAT(data.date, %%s) AS datetime, data.value, data.qc",
+    "FROM series",
+    "INNER JOIN 1%s_%s_%s AS data ON data.data_id = series.data_id",
+    "INNER JOIN stations ON ",
+    "WHERE stations.code IN (%s) "),
     time.period,
     station.type,
     element.name,
-    paste(seriesID, collapse = ","))
+    paste(stationIDs, collapse = ","))
   
   query.safe <- dbEscapeStrings(db, query)
   query.safe <- sprintf(query.safe, "'%Y%m%d%H%i%s'")
@@ -185,9 +187,6 @@ db.select.timeseries <- function(db, seriesID, time.period, station.type, elemen
   
   # Clean up
   dbClearResult(data.ref)
-
-  # Create a factor of the ID's so we can use it in by() later on
-  seriesID.factor <- factor(result$id)
   
   #------------------------------#
   ### Create the master object ###
@@ -207,7 +206,7 @@ db.select.timeseries <- function(db, seriesID, time.period, station.type, elemen
   # Get the right name for this list of timeseries
   namely <- c("hourly", "daily", "monthly", "yearly")[which(time.period == supported.time.periods)]
   
-  obj[namely] <- list(by(result, seriesID.factor, function(x) {
+  obj[namely] <- list(by(result, factor(result$id), function(x) {
     
     dt <- data.table(datetime = x$datetime, value = x$value)
     setkey(dt, datetime)
@@ -233,9 +232,57 @@ db.select.timeseries <- function(db, seriesID, time.period, station.type, elemen
     return(dt)
   }))
   
+  #--------------------------------#
+  ### Query the DB for meta data ###
+  #--------------------------------#
   
+  query <- sprintf(paste(
+    "SELECT",
+    "series.data_id AS id, stations.code AS station_code, name,",
+    "types.type_id AS type_id, elements.element_id AS element_id,",
+    "element_group, type, scale, unit, types.description AS type_desc,",
+    "latitude, longitude, elevation, aggregation, period, element",
+    "FROM series",
+    "INNER JOIN stations ON stations.code = series.code AND stations.type_id = series.type_id",
+    "INNER JOIN types ON types.type_id = series.type_id",
+    "INNER JOIN elements ON elements.element_id = series.element_id",
+    "WHERE series.data_id IN (%s)"
+  ),paste(seriesID, collapse = ","))
   
-  return(result)
+  query.safe <- dbEscapeStrings(db, query)
+  data.ref <- dbSendQuery(db, query.safe)
+  result <- dbFetch(data.ref, n = -1)
+  
+  dbClearResult(data.ref)
+  
+  obj$meta <- by(result, factor(result$id), function(x){
+    
+    meta <- list (
+      sta_id = unique(x$station_code),
+      sta_name = unique(x$name),
+      sta_lat = unique(x$latitude),
+      sta_lon = unique(x$longitude),
+      sta_elev = unique(x$elevation),
+      sta_type = ifelse(unique(x$type) == "H", "AWS", "MAN"),
+      area_sta_id = unique(x$station_code),
+      area_lat = unique(x$latitude),
+      area_lon = unique(x$longitude),
+      area_radius = 0,
+      area_cat = 0,
+      var_id = unique(x$element),
+      var_name = unique(x$element_group),
+      var_unit = unique(x$unit),
+      var_scale = unique(x$scale),
+      var_period = unique(x$aggregation)
+      #ser_current = x[nrow(x), "date"]
+    )
+    class(meta) <- cfg$obj.timeseries.meta.class
+    return(meta)
+  })
+  
+
+  
+  return(obj)
 }
 
 db.insert.timeseries <- function(db, timeseries, time.period, station.type, element.name) {
