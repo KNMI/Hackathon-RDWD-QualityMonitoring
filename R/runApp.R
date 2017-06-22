9660a893928a8bf16fcf67dac96c3fc472941906
MartinRoth <marsianus@gmail.com>
2017-06-22 16:13:31 +0200

    Ensure that wd stays the same

--- R/runApp.R
+++ R/runApp.R
@@ -1,5 +1,7 @@
 #' Run app
 #' @export
 runQualityMonitoring <- function() {
+  wd <- getwd()
   runApp("inst/shiny", launch.browser = TRUE)
+  on.exit(setwd(wd))
 }
--- inst/shiny/server.R
+++ inst/shiny/server.R
@@ -190,7 +190,7 @@ server <- function(input, output, session) {
           lng = ~ longitude,
           popup = ~ name,
           layerId = ~ code_real,
-      #    label = ~type_id,
+          label = ~type_id,
           color = ~pal(type_id))
     }
   })
@@ -204,7 +204,7 @@ server <- function(input, output, session) {
           lng = ~ longitude,
           popup = ~ name,
           layerId = ~ code_real,
-       #   label = ~type_id,
+          label = ~type_id,
           color = ~pal(type_id))
     }
   })
