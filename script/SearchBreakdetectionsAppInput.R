9d3dc0d9e46375dd2c05c9cab6877ec744cfd177
LottedeVos <lotte.devos@wur.nl>
2017-06-23 07:30:15 +0000

    script with function to plot selected station versus selected subset of stations (both MAN and AWS), as well as the radar at those locations. As the function to obtain yearly values from the database based on station IDs was not yet working properly, a temporary method is used to select stations. This takes more time than strictly needed, but does not affect the outcome.

--- script/SearchBreakdetectionsAppInput.R
+++ script/SearchBreakdetectionsAppInput.R
@@ -6,15 +6,31 @@
 #' @title Break detection of app input
 #' @description Function plots relative differences of station with selected subset MAN and Radar values, where breaks are clearly identified. 
 #' @details The function generates timelines of relative precipitation differences, as well as breakpoints (as compared to stations measurements), and visualizes this in 5 plots: full year and seasonal. The plot automatically visualises the entire period that is available. 
-#' @param 
-#' @param all.stations default is "TRUE" means all stations are aggregated. If all.stations = FALSE, an array of sta_ID needs to be provided. 
-#' @example 
+#' @param station first row of output from App. This row should contain information from which type and element can be derived. 
+#' @param subsetselection table with rows of output from App. Each row should contain information from which type and element can be derived.
+#' @example BreakDetectionFinder(stationID = "260_H", subsetselectionIDs = c("10_N",  "11_N",  "12_N",  "13_N", "220_H"))
 #' @author Lotte
-#' @export
+#' @export 5 plots of relative differences between the selected station and the average of the selected subset station values as well as the radar at those locations, with red indicators if a break is found. 
+
+# keep this in working memory if query doesn't work. Warning: takes a long!
+obj <- db.execute(db.select.all, time.interval="1hour", type="H", element="RH")  # AWS
+obj <- aggregate.to.88.2(obj)
+obj <- aggregate.to.seasonal.2(obj)
+obj <- aggregateTo.year(obj) 
+obj2 <- db.execute(db.select.all, time.interval="1day", type="N", element="RD")  # MAN
+obj2 <- aggregate.to.seasonal.2(obj2)
+obj2 <- aggregateTo.year(obj2) 
+obj3a <- db.execute(db.select.all, time.interval="1hour", type="H", element="RR") # Radar at all locations
+obj3b <- db.execute(db.select.all, time.interval="1hour", type="N", element="RR") # Radar at all locations
+obj3a <- aggregate.to.88.2(obj3a)
+obj3a <- aggregate.to.seasonal.2(obj3a)
+obj3a <- aggregateTo.year(obj3a) 
+obj3b <- aggregate.to.88.2(obj3b)
+obj3b <- aggregate.to.seasonal.2(obj3b)
+obj3b <- aggregateTo.year(obj3b) 
 
 
-
-BreakDetectionFinder <- function(stationname, subsetselection){
+BreakDetectionFinder <- function(stationID, subsetselectionIDs){
 
 Sys.setenv(R_CONFIG_ACTIVE = "test")
 source("R/databaseOperations.R")
@@ -25,84 +41,46 @@ source("R/breakDetection.R")
 cfg <- config::get(file = "config/config.yml")
 
 # Data input # 
+station.ID <- substr(stationID, 1, (nchar(stationID)-2))
+type.station <- substr(stationID, nchar(stationID), nchar(stationID))
+if(type.station=="H"){
+  element.station <- "RH"}
+if(type.station=="N"){
+  element.station <- "RD"}
 
-# find obj corresponding with stationname:
 
-db.select.timeseries <- function(db, station.IDs, time.interval, type, element) {
-obj <- db.execute(db.select.timeseries, time.interval)
-  
-obj <- db.execute(db.select.all, time.interval="1hour", type="H", element="RH")  # AWS
-obj <- aggregate.to.88.2(obj)
-obj <- aggregate.to.seasonal.2(obj)
-obj <- aggregateTo.year(obj) 
-obj2 <- db.execute(db.select.all, time.interval="1day", type="N", element="RD")  # MAN
-obj2 <- aggregate.to.seasonal.2(obj2)
-obj2 <- aggregateTo.year(obj2) 
+subset.IDs <- substr(subsetselectionIDs, 1, (nchar(subsetselectionIDs)-2))
+type.subset <- substr(subsetselectionIDs, nchar(subsetselectionIDs), nchar(subsetselectionIDs))  
+element.subset <- rep(NA, length(type.subset))
+ element.subset[which(type.subset == "H")] <- "RH" 
+ element.subset[which(type.subset == "N")] <- "RD"   
 
-MAN_labels2 <- sapply(obj2$year$meta, function(m){m$sta_id})
-MAN_labels <- c()
-for(m in 1:length(MAN_labels2)){MAN_labels[m] <- MAN_labels2[[m]]}
+########### 
+ # find relevant values in obj. This is a temporary solution until we can make a query to obtain these values directly from the database. 
+#station selection 
+ if(type.station == "H"){
+ obj1selec <- sapply(obj$year$meta, function(m){m$sta_id == station.ID})
+ obj_subset1_y <- obj$year$data[obj1selec]  # list with 1 data.table. 
+ obj_subset1_season <- obj$season$data[obj1selec]}  # list with 1 data.table. 
+ if(type.station =="N"){
+ obj1selec <- sapply(obj2$year$meta, function(m){m$sta_id == station.ID})
+ obj_subset1_y <- obj2$year$data[obj1selec]  # list with 1 data.table. 
+ obj_subset1_season <- obj2$season$data[obj1selec]}  # list with 1 data.table. 
 
-obj3 <- db.execute(db.select.all, time.interval="1hour", type="N", element="RR") # Radar at MAN locations
-obj3selec <- sapply(obj3$`1hour`$meta, function(m){m$sta_id %in% MAN_labels & m$sta_type=="n"})
-obj3$`1hour`$data <- obj3$`1hour`$data[obj3selec]   #only take the radar timeseries at MAN stations
-obj3$`1hour`$meta <- obj3$`1hour`$meta[obj3selec]   
-obj3 <- aggregate.to.88.2(obj3)
-obj3 <- aggregate.to.seasonal.2(obj3)
-obj3 <- aggregateTo.year(obj3) 
+#subset selection from subset AWS and MAN
+ obj2subsetselec1 <- sapply(obj$year$meta, function(m){m$sta_id %in% subset.IDs})
+ obj2subsetselec2 <- sapply(obj2$year$meta, function(m){m$sta_id %in% subset.IDs})
+ obj_subset2_y <- c(obj$year$data[obj2subsetselec1] , obj2$year$data[obj2subsetselec2]) 
+ obj_subset2_season <- c(obj$season$data[obj2subsetselec1] , obj2$season$data[obj2subsetselec2]) 
+ 
+#subset selection from obj3a and obj3b for radar
+ obj3subsetselec1 <- sapply(obj3a$year$meta, function(m){m$sta_id %in% subset.IDs})
+ obj3subsetselec2 <- sapply(obj3b$year$meta, function(m){m$sta_id %in% subset.IDs})
+ obj_subset3_y <- c(obj3a$year$data[obj3subsetselec1] , obj3b$year$data[obj3subsetselec2])
+ obj_subset3_season <- c(obj3a$season$data[obj3subsetselec1] , obj3b$season$data[obj3subsetselec2])
 
-AWS_series <- names(obj$year$meta)
-AWS_labels <- sapply(obj$year$meta, function(m){paste0(m$sta_id, "_H")})
+##############  
 
-emptycol <- rep(NA,(length(AWS_labels)+1))
-#BD_output <- data.table(Comb_Name=emptycol, year=emptycol, djf=emptycol, mam=emptycol, jja=emptycol, son=emptycol )
-
-BD_output <- rbindlist(lapply(0:length(AWS_labels), function(n) {
-  
-  if(n == 0) {
-    
-    Comb_Name <- "NL"     # all AWS vs all MAN vs all Radar
-    #  obj_subset1 <- obj
-    #  obj_subset2 <- obj2
-    #  obj_subset3 <- obj3
-    
-    obj_subset1_y <- obj$year$data  # list with 1 data.table. 
-    obj_subset1_season <- obj$season$data  # list with 1 data.table. 
-    
-    obj_subset2_y <- obj2$year$data
-    obj_subset2_season <- obj2$season$data
-    
-    obj_subset3_y <- obj3$year$data
-    obj_subset3_season <- obj3$year$data
-    
-  }else{
-    Comb_Name <- AWS_labels[[n]]
-    obj_subset1_y <- obj$year$data[n]  # list with 1 data.table. 
-    obj_subset1_season <- obj$season$data[n]  # list with 1 data.table. 
-    
-    stations_nearby <- station.nearby(AWS_labels[[n]])$nearby_code_real
-    # in case no nearby stations are available
-    if(length(stations_nearby)==0){
-      BD_output$year[(n+1)] <- cfg$database.na.value
-      BD_output$djf[(n+1)] <- cfg$database.na.value
-      BD_output$mam[(n+1)] <- cfg$database.na.value
-      BD_output$jja[(n+1)] <- cfg$database.na.value
-      BD_output$son[(n+1)] <- cfg$database.na.value
-    }else{
-      
-      nearby_labels <- substr(stations_nearby, 1, (nchar(stations_nearby)-2))
-      
-      selec_obj2_y <- sapply(obj2$year$meta, function(m){m$sta_id %in% substr(stations_nearby, 1, nchar(stations_nearby)-2) }) 
-      obj_subset2_y <- obj2$year$data[selec_obj2]
-      selec_obj2_season <- sapply(obj2$season$meta, function(m){m$sta_id %in% substr(stations_nearby, 1, nchar(stations_nearby)-2) }) 
-      obj_subset2_season <- obj2$season$data[selec_obj2]
-      
-      selec_obj3_y <- sapply(obj3$year$meta, function(m){m$sta_id %in% substr(stations_nearby, 1, nchar(stations_nearby)-2) }) 
-      obj_subset3_y <- obj3$year$data[selec_obj3]
-      selec_obj3_season <- sapply(obj3$year$meta, function(m){m$sta_id %in% substr(stations_nearby, 1, nchar(stations_nearby)-2) }) 
-      obj_subset3_season <- obj3$year$data[selec_obj3]
-    }}
-  
   # Spatial averaging #
   
   obj1_average_y  <- average.spatial(timeseries=obj_subset1_y) 
@@ -144,38 +122,65 @@ BD_output <- rbindlist(lapply(0:length(AWS_labels), function(n) {
   rel_dif_AWSvsRAD_son <- timeseries.relative.difference(timeserie1=obj1_average_son, timeserie2=obj3_average_son)
   
   
-  BDyear <- break.detection(rel_dif_AWSvsMAN_y,name=Comb_Name,type='y',plot.score = TRUE)
-  BDdjf <- break.detection(rel_dif_AWSvsMAN_djf,name=Comb_Name,type='djf',plot.score = TRUE)
-  BDmam <- break.detection(rel_dif_AWSvsMAN_mam,name=Comb_Name,type='mam',plot.score = TRUE)
-  BDjja <- break.detection(rel_dif_AWSvsMAN_jja,name=Comb_Name,type='jja',plot.score = TRUE)
-  BDson <- break.detection(rel_dif_AWSvsMAN_son,name=Comb_Name,type='son',plot.score = TRUE)
+  BDyear <- break.detection(rel_dif_AWSvsMAN_y,name=Comb_Name,type='y',plot.score = FALSE)
+  BDdjf <- break.detection(rel_dif_AWSvsMAN_djf,name=Comb_Name,type='djf',plot.score = FALSE)
+  BDmam <- break.detection(rel_dif_AWSvsMAN_mam,name=Comb_Name,type='mam',plot.score = FALSE)
+  BDjja <- break.detection(rel_dif_AWSvsMAN_jja,name=Comb_Name,type='jja',plot.score = FALSE)
+  BDson <- break.detection(rel_dif_AWSvsMAN_son,name=Comb_Name,type='son',plot.score = FALSE)
   
   
-  if(length(BDyear) > 0 | length(BDdjf) > 0 | length(BDmam) > 0 | length(BDjja) > 0 | length(BDson) > 0 ) {
+  dev.new() #year
+  if(length(BDyear) == 0){ main <- paste0("Year: no breaks detected")
+  }else{ main <- paste0("Year: breaks detected")}
+  plot(as.numeric(substr(rel_dif_AWSvsMAN_y$datetime, 1,4)), rel_dif_AWSvsMAN_y$value, type="l", xlab="Time", ylab="Relative difference [%]", main=main, 
+       ylim=range(c(rel_dif_AWSvsMAN_y$value,rel_dif_AWSvsRAD_y$value), na.rm=T), xlim=c(1981, 2017))
+  lines(as.numeric(substr(rel_dif_AWSvsRAD_y$datetime, 1,4)), rel_dif_AWSvsRAD_y$value, col="purple")
+  abline(v=BDyear, col="red")
+  abline(h=0, col="grey")
+  legend("bottomleft", c("station vs other stations", "station vs radar", "break"), col=c("black", "purple", "red"), lty=1)
+  
+  dev.new() #djf
+  if(length(BDdjf) == 0){ main <- paste0("Winter: no breaks detected")
+  }else{ main <- paste0("Winter: breaks detected")}
+  plot(as.numeric(substr(rel_dif_AWSvsMAN_djf$datetime, 1,4)), rel_dif_AWSvsMAN_djf$value, type="l", xlab="Time", ylab="Relative difference [%]", main=main, 
+       ylim=range(c(rel_dif_AWSvsMAN_djf$value,rel_dif_AWSvsRAD_djf$value), na.rm=T), xlim=c(1981, 2017))
+  lines(as.numeric(substr(rel_dif_AWSvsRAD_djf$datetime, 1,4)), rel_dif_AWSvsRAD_djf$value, col="purple")
+  abline(v=BDdjf, col="red")
+  abline(h=0, col="grey")
+  legend("bottomleft", c("station vs other stations", "station vs radar", "break"), col=c("black", "purple", "red"), lty=1)
+  
+  dev.new() #mam
+  if(length(BDmam) == 0){ main <- paste0("Spring: no breaks detected")
+  }else{ main <- paste0("Spring: breaks detected")}
+  plot(as.numeric(substr(rel_dif_AWSvsMAN_mam$datetime, 1,4)), rel_dif_AWSvsMAN_mam$value, type="l", xlab="Time", ylab="Relative difference [%]", main=main, 
+       ylim=range(c(rel_dif_AWSvsMAN_mam$value,rel_dif_AWSvsRAD_mam$value), na.rm=T), xlim=c(1981, 2017))
+  lines(as.numeric(substr(rel_dif_AWSvsRAD_mam$datetime, 1,4)), rel_dif_AWSvsRAD_mam$value, col="purple")
+  abline(v=BDmam, col="red")
+  abline(h=0, col="grey")
+  legend("bottomleft", c("station vs other stations", "station vs radar", "break"), col=c("black", "purple", "red"), lty=1)
+  
+  dev.new() #jja
+  if(length(BDjja) == 0){ main <- paste0("Summer: no breaks detected")
+  }else{ main <- paste0("Summer: breaks detected")}
+  plot(as.numeric(substr(rel_dif_AWSvsMAN_jja$datetime, 1,4)), rel_dif_AWSvsMAN_jja$value, type="l", xlab="Time", ylab="Relative difference [%]", main=main, 
+       ylim=range(c(rel_dif_AWSvsMAN_jja$value,rel_dif_AWSvsRAD_jja$value), na.rm=T), xlim=c(1981, 2017))
+  lines(as.numeric(substr(rel_dif_AWSvsRAD_jja$datetime, 1,4)), rel_dif_AWSvsRAD_jja$value, col="purple")
+  abline(v=BDjja, col="red")
+  abline(h=0, col="grey")
+  legend("bottomleft", c("station vs other stations", "station vs radar", "break"), col=c("black", "purple", "red"), lty=1)
+  
+  dev.new() #son
+  if(length(BDson) == 0){ main <- paste0("Autumn: no breaks detected")
+  }else{ main <- paste0("Autumn: breaks detected")}
+  plot(as.numeric(substr(rel_dif_AWSvsMAN_son$datetime, 1,4)), rel_dif_AWSvsMAN_son$value, type="l", xlab="Time", ylab="Relative difference [%]", main=main, 
+       ylim=range(c(rel_dif_AWSvsMAN_son$value,rel_dif_AWSvsRAD_son$value), na.rm=T), xlim=c(1981, 2017))
+  lines(as.numeric(substr(rel_dif_AWSvsRAD_son$datetime, 1,4)), rel_dif_AWSvsRAD_son$value, col="purple")
+  abline(v=BDson, col="red")
+  abline(h=0, col="grey")
+  legend("bottomleft", c("station vs other stations", "station vs radar", "break"), col=c("black", "purple", "red"), lty=1)
     
-    return(
-      data.table(
-        Comb_Name = Comb_Name,
-        year = paste(BDyear, collapse = ","),
-        djf <- paste(BDdjf, collapse = ","),
-        mam <- paste(BDmam, collapse = ","),
-        jja <- paste(BDjja, collapse = ","),
-        son <- paste(BDson, collapse = ",")
-      )
-    )
-  } else {
-    return(NULL)
-  }
+  } #end function
   
-}))
-
-db.execute(db.insert.breakdetection.results, BD_output)
-
-
-
-
-
-} # end function
 
 
 
